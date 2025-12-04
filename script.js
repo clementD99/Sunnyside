f19d0534ca8751b7ff733499c968dfe32bd6fecd
clementD99 <clement.desmur@gmail.com>
2025-12-04 15:13:00 +0100

    JS pour les cards

---
+++ script.js
@@ -0,0 +1,39 @@
+const testimonialsList = document.getElementById("testimonials-list");
+let clients = [];
+
+const fetchClients = async () => {
+  try {
+    const response = await fetch("clients.json");
+    const clients = await response.json();
+    return clients;
+  } catch (error) {
+    console.error("error:", error);
+  }
+};
+
+function displayClients(clients) {
+  testimonialsList.innerHTML = "";
+  clients.forEach((client) => {
+    const element = document.createElement("div");
+    element.classList.add("quotes");
+    element.dataset.name = client.name;
+
+    element.innerHTML = `
+    <img src=${client.photo} alt="${client.photo}">
+    <p class="comment">${client.comment}</p>
+    <div class="bottom-text">
+    <h3 class="name">${client.name}</h3>
+    <span class="profession">${client.profession}</span>
+    </div>
+    </div>`;
+
+    testimonialsList.appendChild(element);
+  });
+}
+
+fetchClients().then((clients) => {
+  if (clients) {
+    element = clients;
+    displayClients(element);
+  }
+});
